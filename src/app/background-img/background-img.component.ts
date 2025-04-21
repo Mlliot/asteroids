0399d912660391dc965b20f3d0411f0415123e77
mmart27 <edwin.martinez@capgemini.com>
2025-04-21 16:38:44 -0400

    Working on API call

--- src/app/background-img/background-img.component.ts
+++ src/app/background-img/background-img.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { ApiService } from '../services/api.service';
@@ -11,17 +11,60 @@ import { CommonModule } from '@angular/common';
   styleUrl: './background-img.component.css'
 })
 export class BackgroundImgComponent {
+  key: string = '';
+  service = inject(ApiService);
+  apiResponse: any
   apodData: any
 
-  constructor( private apiService: ApiService) { }
+  myArr: string[] = new Array()
+
+  constructor(private apiService: ApiService) { }
 
   // Starts up api call and subscribes to get data
+  // ngOnInit(): void {
+  //   console.log('hi')
+  //   this.apiService.getToday().subscribe(data => {
+  //     console.log(data)
+  //     this.apodData = data;
+  //   });
+  // }
+
   ngOnInit(): void {
-    console.log('hi')
-    this.apiService.getToday().subscribe(data => {
-      console.log(data)
-      this.apodData = data;
+    this.key = this.service.getKey();
+    console.log(this.key);
+    this.service.getClosest().subscribe((test) => {
+      // console.log(test);
+      this.apiResponse = test
+      console.log(JSON.stringify(this.apiResponse.near_earth_objects) + 'hello')
+      this.myArr.push(this.apiResponse.near_earth_objects)
     });
+    //console.log(this.service.getToday());
+    // console.log(this.apiResponse)
+  }
+
+  getFiveResponses() {
+
+    // for (const date in this.myArr) {
+    //   if (this.myArr.hasOwnProperty(date)) {
+    //     console.log(`Date: ${date}, Array:`, this.myArr[date]);
+    //     // You can also iterate through the array elements if needed
+    //     this.myArr[date].forEach((item: any, index: any) => {
+    //       console.log(`  Item ${index}:`, item);
+    //     });
+    //   }
+    // }
+    let value = ''
+    for (let key in this.myArr) {
+      if (this.myArr.hasOwnProperty(key)) {
+          value = this.myArr[key];
+          // for(let i = 0; i < value.length; i++) {
+          //   console.log( value[i] + ' yo')
+          // }
+          //console.log(key, value, 'hello');
+      }
+  }
+
+    console.log(this.myArr[0])
   }
 
 
